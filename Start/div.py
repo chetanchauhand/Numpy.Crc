7e472bdddd36683f3724dace2802591d9b22d4ad
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-05 23:25:23 +0530

    last

---
+++ Start/div.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a1 = np.array([10,20,30,40,50])
+a2 = np.array([5,10,15,20,25])
+
+print("Array1 is :",a1)
+print("Array2 is :",a2)
+
+div = np.divide(a1,a2)
+print("Dvision result of both:",div)
