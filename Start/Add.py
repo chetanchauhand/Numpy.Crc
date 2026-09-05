6034fb7223000f27156e09f369cebea8a0f1dd61
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-05 23:04:24 +0530

    4th

--- Start/Add.py
+++ Start/Add.py
@@ -8,5 +8,6 @@ print("array 1 : ",a1)
 print("array 2 : ",a2)
 
 # rasarr = np.sum([a1,a2])
-rasarr = np.sum([a1,a2],axis=0)
+# rasarr = np.sum([a1,a2],axis=0)
+rasarr = np.sum([a1,a2],axis=1)
 print("Sum of array 1&2 is :",rasarr)
