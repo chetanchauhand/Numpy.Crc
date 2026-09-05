32c96fba4a4b07d99ed32ac567751c1383542ceb
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-05 22:59:28 +0530

    2nd

---
+++ Start/Add.py
@@ -0,0 +1,11 @@
+# Add two arrays 
+import numpy as np
+
+a1 = np.array([10,12,12,13,15])
+a2 = np.array([17,19,30,20,21])
+
+print("array 1 : ",a1)
+print("array 2 : ",a2)
+
+rasarr = np.sum([a1,a2])
+print("Sum of array 1&2 is :",rasarr)
--- Start/tempCodeRunnerFile.py
+++ Start/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-....:
+p.sum(a1,a2)
+# print("Sum of array 1&2 is :",add)
