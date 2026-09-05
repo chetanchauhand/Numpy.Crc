67e1fbe26b921aa6fcaf6191ded6eff3acf105fc
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-05 23:11:29 +0530

    4th

---
+++ Start/Sub.py
@@ -0,0 +1,10 @@
+# Subtract two array
+import numpy as np
+
+a1 = np.array([10,11,14,19,30])
+a2 = np.array([9,8,7,6,11])
+print("Array 1 is :",a1)
+print("Array 2 is :",a2)
+
+Sub = np.subtract(a1 , a2)
+print("Difference between Array 1 & 2",Sub)
