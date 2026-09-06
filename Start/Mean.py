aa47155ed32790c575dada9cef6fe74b1a600529
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-06 10:33:15 +0530

    6th

---
+++ Start/Mean.py
@@ -0,0 +1,13 @@
+# Find mean , median value in numpy array
+import numpy as np
+a1 = np.array([10,20,30,40,50])
+a2 = np.array([15,25,35,45,55])
+
+print("Array 1 is :",a1)
+print("Array 2 is :",a2)
+
+asarr1 = np.mean(a1)
+asarr2 = np.mean(a2)
+
+print("\n Mean of Array 1 is :",asarr1)
+print("\n Mean of Array 2 is :",asarr2)
