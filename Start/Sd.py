95da1cff93b6acaf3a6798c9912cb0c8f912886e
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-06 10:43:21 +0530

    9th

---
+++ Start/Sd.py
@@ -0,0 +1,15 @@
+# Find Standard Deviation of numpy array
+
+import numpy as np
+
+a1 = np.array([10,20,30,40,50,60])
+a2 = np.array([12,14,16,18,22,24])
+
+print("Array1 is :",a1)
+print("Array2 is :",a2)
+
+asarr1 = np.std(a1)
+asarr2 = np.std(a2)
+
+print("Std of Array1 is :",asarr1)
+print("Std of Arrat2 is :",asarr2)
