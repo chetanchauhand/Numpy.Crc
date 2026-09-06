c04035f895d9676db82dea642ca88bba35ca160c
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-06 10:57:20 +0530

    9th

--- Start/Gen.py
+++ Start/Gen.py
@@ -1,5 +1,10 @@
 # Generate random number in numpy
 from numpy import random
-a = random.randint(5)
+# a = random.randint(5)
 
-print("Random value is:",a)
+# print("Random value is:",a)
+
+# Generate random number (Fix size) in numpy.
+
+a = random.randint(10,size=(5))
+print("Fix size of random num is :",a)
