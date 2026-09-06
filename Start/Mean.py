86252d87e9798197b0c365606dfcefac987ee146
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-06 10:35:20 +0530

    7th

--- Start/Mean.py
+++ Start/Mean.py
@@ -6,8 +6,10 @@ a2 = np.array([15,25,35,45,55])
 print("Array 1 is :",a1)
 print("Array 2 is :",a2)
 
-asarr1 = np.mean(a1)
-asarr2 = np.mean(a2)
+# asarr1 = np.mean(a1)
+# asarr2 = np.mean(a2)
+asarr1 = np.median(a1)
+asarr2 = np.median(a2)
 
 print("\n Mean of Array 1 is :",asarr1)
 print("\n Mean of Array 2 is :",asarr2)
