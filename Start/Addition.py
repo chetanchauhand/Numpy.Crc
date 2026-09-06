3c051ba68cb80c71367c8b315365aefe8d04b657
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-06 10:14:13 +0530

    1st

---
+++ Start/Addition.py
@@ -0,0 +1,14 @@
+# Addition operation in numpy array
+import numpy as np
+
+a1 = np.array([10,20,30,40,50])
+a2 = np.array([11,22,33,44,55])
+
+print("Array1 :",a1)
+print("Array2 :",a2)
+
+a1 = a1 + 10
+a2 = a2 + 11
+
+print("Updated Array 1 is :",a1)
+print("Updated Array 2 is :",a2)
