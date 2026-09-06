f30a5da506b06fef44568e0ff283d8e952ddce1f
chetanchauhan5546 <chetanchauhan5546@gmail.com>
2026-09-06 10:15:32 +0530

    2nd

--- Start/Addition.py
+++ Start/Addition.py
@@ -7,8 +7,12 @@ a2 = np.array([11,22,33,44,55])
 print("Array1 :",a1)
 print("Array2 :",a2)
 
-a1 = a1 + 10
-a2 = a2 + 11
+# a1 = a1 + 10
+# a2 = a2 + 11
+
+a1 = a1 - 10
+a2 = a2 - 11
+
 
 print("Updated Array 1 is :",a1)
 print("Updated Array 2 is :",a2)
